17848c9d7cd630fc917acbac43b68fb759429a47
KMinh <daukhacminh006@gmail.com>
2024-02-13 22:41:06 +0700

    seo

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,14 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import compress from 'astro-compress';
+import robotsTxt from 'astro-robots-txt';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
   experimental: {
     viewTransitions: true,
   },
-  integrations: [tailwind(), compress()],
+  site: 'https://www.thomasinvestmentbank.com',
+  integrations: [tailwind(), compress(), robotsTxt(), sitemap()],
 });
