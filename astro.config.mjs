b7f06d5d0681dfa40535e1a6a59f95fc1124f414
KMinh <daukhacminh006@gmail.com>
2024-02-15 15:10:01 +0700

    pricing

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,15 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import compress from 'astro-compress';
 import robotsTxt from 'astro-robots-txt';
-
-import sitemap from "@astrojs/sitemap";
+import vercel from '@astrojs/vercel/serverless';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
   experimental: {
-    viewTransitions: true
+    viewTransitions: true,
   },
+  output: 'hybrid',
   site: 'https://www.thomasinvestmentbank.com',
-  integrations: [tailwind(), compress(), robotsTxt(), sitemap()]
+  integrations: [tailwind(), compress(), robotsTxt(), sitemap(), vercel()],
 });
---
+++ src/pages/api/signup.json.js
@@ -0,0 +1,56 @@
+import Mailjet from 'node-mailjet';
+
+export const prerender = false;
+
+async function sendEmail(email, name) {
+  const mailjet = Mailjet.apiConnect(import.meta.env.MJ_APIKEY, import.meta.env.MJ_SECRET);
+
+  try {
+    const send = await mailjet.post('send', { version: 'v3.1' }).request({
+      Messages: [
+        {
+          From: {
+            Email: 'daukhacminh006@gmail.com',
+            Name: 'Thomas Investment Bank',
+          },
+          To: [
+            {
+              Email: 'daukhacminh006@gmail.com',
+              Name: 'CEO',
+            },
+          ],
+          Subject: 'TIB New Sign Up',
+          TemplateID: 5697483,
+          TemplateLanguage: true,
+          Variables: {
+            name: name,
+            email: email,
+          },
+        },
+      ],
+    });
+
+    console.log(send.body.Messages[0].To[0].Email);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+export const POST = async ({ request }) => {
+  const data = await request.formData();
+  const name = data.get('name');
+  const email = data.get('email');
+
+  await sendEmail(email, name);
+
+  return new Response(
+    JSON.stringify({
+      message: 'success',
+    }),
+    {
+      status: 200,
+    }
+  );
+
+  return new Response(null, { status: 400 });
+};
